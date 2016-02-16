e86f39ccf90bd25cd038ff8ecc6cccc34fa1fdc2
Max <maksymkharuk@iesebre.com>
2016-02-16 19:20:42 +0100

    Added enable bluetooth functionality

--- app/src/main/java/com/dam2/max/bluetoothtinkeringandroid/MainActivity.java
+++ app/src/main/java/com/dam2/max/bluetoothtinkeringandroid/MainActivity.java
@@ -1,11 +1,18 @@
 package com.dam2.max.bluetoothtinkeringandroid;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final String TAG = "BluetoothTinkering MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        isBluetoothAvailable();
+        if (!isBluetoothAvailable()) { return; }
 
 
     }
+
+
 
     /**
      * Check if bluetooth is available
@@ -25,8 +35,6 @@ public class MainActivity extends AppCompatActivity {
      */
     private boolean isBluetoothAvailable()
     {
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
         if (mBluetoothAdapter != null) { return true; }
         else {
             Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_SHORT).show();
@@ -34,10 +42,62 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Call enable bluetooth system Intent
+     */
+    private void checkBluetoothEnabled()
+    {
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    /**
+     * Enable bluetooth on activity result actions
+     * @param resultCode code of petition result
+     */
+    private void enableBluetoothResult(int resultCode)
+    {
+        // When the request to enable Bluetooth returns
+        if (resultCode == Activity.RESULT_OK) {
+            // Bluetooth is now enabled, so set up a chat session
+            //setupChat();
+            Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_SHORT).show();
+        } else {
+            // User did not enable Bluetooth or an error occurred
+            Log.d(TAG, "BT not enabled");
+            Toast.makeText(this, "Bluetooth not enabled", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Call enable bluetooth system Intent
+        checkBluetoothEnabled();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode)
+        {
+            case REQUEST_ENABLE_BT:
+                enableBluetoothResult(resultCode);
+                break;
+        }
 
     }
 }
