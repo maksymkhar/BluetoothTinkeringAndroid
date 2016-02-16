50aed13bb96f33fb4f256c6dd5aa702fec3a5033
Max <maksymkharuk@iesebre.com>
2016-02-16 19:10:19 +0100

    Added bluetooth available comprovation

--- app/src/main/java/com/dam2/max/bluetoothtinkeringandroid/MainActivity.java
+++ app/src/main/java/com/dam2/max/bluetoothtinkeringandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dam2.max.bluetoothtinkeringandroid;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        isBluetoothAvailable();
+
+
+    }
+
+    /**
+     * Check if bluetooth is available
+     * @return Bluetooth available
+     */
+    private boolean isBluetoothAvailable()
+    {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBluetoothAdapter != null) { return true; }
+        else {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
     }
 }
