e4578da1bd28064439dd0e2461e32b7b613be89f
Max <maksymkharuk@iesebre.com>
2016-02-16 19:50:21 +0100

    Added paired devices method and menu option

--- app/src/main/java/com/dam2/max/bluetoothtinkeringandroid/MainActivity.java
+++ app/src/main/java/com/dam2/max/bluetoothtinkeringandroid/MainActivity.java
@@ -2,14 +2,16 @@ package com.dam2.max.bluetoothtinkeringandroid;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,51 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
         if (!isBluetoothAvailable()) { return; }
 
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        // Call enable bluetooth system Intent
-        checkBluetoothEnabled();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode)
-        {
-            case REQUEST_ENABLE_BT:
-                enableBluetoothResult(resultCode);
-                break;
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId())
-        {
-            case R.id.menu_discover:
-                callDiscoverableIntent();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -127,5 +84,73 @@ public class MainActivity extends AppCompatActivity {
         Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
         discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
         startActivity(discoverableIntent);
+    }
+
+    private void getPairedDevices()
+    {
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                Log.v("Paired device", device.getName());
+            }
+
+            Toast.makeText(this, pairedDevices.size() + " devices paired.", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "No devices paired.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode)
+        {
+            case REQUEST_ENABLE_BT:
+                enableBluetoothResult(resultCode);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.menu_discover:
+                callDiscoverableIntent();
+                break;
+            case R.id.menu_paired_devices:
+                getPairedDevices();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Call enable bluetooth system Intent
+        checkBluetoothEnabled();
     }
 }
